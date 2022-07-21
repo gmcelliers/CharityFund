0c3073f361d90f9e3e1d45cce39160bc724faa8a
gmcelliers <57320512+gmcelliers@users.noreply.github.com>
2022-07-21 10:35:51 +0200

    Updated 'charities' page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
 import { AccountComponent } from './account/account.component';
-const routes: Routes = [{path:'', component:LandingComponent},{path:'account',component:AccountComponent}];
+import { HowComponent } from './how/how.component';
+import { CharitiesComponent } from './charities/charities.component';
+
+const routes: Routes = [{path:'', component:LandingComponent},{path:'account',component:AccountComponent},{path:'how', component:HowComponent}, {path:'charities', component:CharitiesComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 import { AccountComponent } from './account/account.component';
+import { HowComponent } from './how/how.component';
+import { CharitiesComponent } from './charities/charities.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
-    AccountComponent
+    AccountComponent,
+    HowComponent,
+    CharitiesComponent
   ],
   imports: [
     BrowserModule,
